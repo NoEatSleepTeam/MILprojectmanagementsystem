629877cf3a65acbaba2049319250c20cb82d4c92
abhishek <abhikori1994@gmail.com>
2016-09-10 15:34:01 +0530

    user sign in view done

--- public/app.js
+++ public/app.js
@@ -1,3 +1,24 @@
 /**
  * Created by abhishek on 10/9/16.
  */
+var app = angular.module('milpms',["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "user_signin.html"
+        })
+        .when("/profile", {
+            templateUrl : "user_profile.html"
+        })
+        .when("/project", {
+            templateUrl : "project.html"
+
+        }).when("/createProject",{
+            templateUrl:"create_project.html"
+        }).otherwise({
+            templateUrl:"not_found.html"
+        });
+});
+
+
